09fe0740731b72de39a20f779d08444ca1ae0d32
ahmer1246 <ahmer1246@gmail.com>
2023-08-31 21:31:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+
+
+// -----CHAPTER NO 1----//
+
+//Q NO 1
+
+// alert('hello world')
+
+//Q NO 2
+
+// alert('Error ! Please enter a valid password')
+
+//Q NO 3 
+
+// var message ="Welcome to JS Land...\n Happy Coding!"
+// alert(message)
+
+//Q NO 4
+
+// var greetMessage = "Happy Coding ! \n Prevent this page from additional dialogs "
+// alert(greetMessage );
+
+//Q NO 5
+// var greetMessage = "Hello.. I can run JS through my web browser's console"
+// alert(greetMessage );
+
+// Q NO 6 
+// DONE 
+// Q NO 7 
+// DONE
+
+                 // CHAPTER NO 2 START
+                               
+                                //  Q No 1
+
+//var greetingMessage ="Dhone Doe";
+//alert(greetingMessage);
+
+                                   //Q No 2
+ // var greetingMessage ="15 Years old";
+//alert(greetingMessage);
+                                   
+
+                                         //Q No 3
+  //var greetingMessage ="Certified Mobile Application Development";
+  //alert(greetingMessage);
+                              
+
+                                         //Q No 4
+
+  //var greetingMessage ="PIZZA\nPIZZ\nPIZ\nPI\nP";
+  //alert(greetingMessage);
+
+                                          //Q No 5
+
+  //var greetingMessage ="email adress ak5589667@gmail.com";
+ // alert(greetingMessage);
+
+                                       //Q No 6
+
+//var greetingMessage ="i am trying to learn form the BOOk A Smarter\nWay to learn javascript";
+//alert(greetingMessage);
+
+                                       //Q No 7
+
+//var greetingMessage ="Yah! i can write HTML content through javasscript";
+//alert(greetingMessage);
+
+ //Q No 8
+
+// var greetingMessage ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+//  alert(greetingMessage);
+                                                                          
